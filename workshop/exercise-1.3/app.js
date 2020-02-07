c8d4a693685e167fc46063e425123954bd4c55cb
Manny Dheer <mannydheer@gmail.com>
2020-02-07 00:05:30 -0500

    Update app.js

--- workshop/exercise-1.3/app.js
+++ workshop/exercise-1.3/app.js
@@ -5,3 +5,35 @@
 // a random amount of time (up to 5 seconds) to click the screen.
 
 // It would be a good idea to create a new function that will manage the whole game.
+
+
+//Lets get a math random number
+
+let random = (Math.floor(Math.random() * 6));
+
+//able to click on anywhere on the body.
+ let body = document.querySelector('body');
+ body.style.height = "100vh";
+//add time into the HTML
+ let countAdd =  document.getElementById('timer');
+ countAdd.innerText = random;
+// ------------------------------------------------------------
+
+let timer = setTimeout(function() {
+    loserOutput = document.getElementById('result');
+    loserOutput.innerText = "LOSER";
+}, random*1000);
+
+
+
+function clicker () {
+    let outputTag = document.getElementById('result');
+    outputTag.innerText = 'WINNER';
+
+}
+
+
+//function call
+ body,addEventListener('click', clicker);
+
+
