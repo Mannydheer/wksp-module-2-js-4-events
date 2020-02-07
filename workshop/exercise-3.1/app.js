53a48bfcd0d69262dd765a3acc29110ca53c435d
Manny Dheer <mannydheer@gmail.com>
2020-02-07 16:47:37 -0500

    javascript 4

---
+++ workshop/exercise-3.1/app.js
@@ -0,0 +1,11 @@
+
+//need to add event listener
+let buttonClick = document.getElementsByClassName('btn');
+buttonClick.addEventListener('click', colorChange);
+
+
+// when you click, call the function below
+
+function colorChange () {
+
+}
