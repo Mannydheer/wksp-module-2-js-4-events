461e971de0c49cd1025b4deab71eae7913d12ed1
Manny Dheer <mannydheer@gmail.com>
2020-02-07 21:57:41 -0500

    javascript 4

--- workshop/exercise-3.1/app.js
+++ workshop/exercise-3.1/app.js
@@ -1,11 +1,36 @@
 
+//for loop will create 20 buttons and create IDS for each. 
+for (let count = 0; count <=20; count ++) {
+    textButton = document.createElement('button');
+    textButton.id = `btn${count}`
+    textButton.innerText = "Button   " + count;
+    document.querySelector('body').appendChild(textButton);
+    //styling for each button as it iterates through the loop
+    textButton.style.backgroundColor = 'red';
+    textButton.style.fontSize = '30px';
+    textButton.style.padding = '20px'
+}
+
+//Now I need to create an event listener that will listen for a button click, when it does, it will
+//turn the button clicked red. 
+
+//create  a loop;
+
+
+    let buttonClick = document.getElementById('`btn${count}`');
+    buttonClick.addEventListener('click', colorChange);
+
+
 //need to add event listener
-let buttonClick = document.getElementsByClassName('btn');
-buttonClick.addEventListener('click', colorChange);
+// let buttonClick = document.getElementById('btn1');
+// buttonClick.addEventListener('click', colorChange);
 
-
-// when you click, call the function below
-
+//var for targetting each Id with the appropriate button. 
+//function call from clicking on the button. 
 function colorChange () {
 
-}
+    buttonClick.style.backgroundColor = 'green';
+ 
+ }
+
+
